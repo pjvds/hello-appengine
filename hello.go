d6f9c64f1f4180a7cb60965957fe4cf59ac8bce4
Pieter Joost van de Sande <pj@born2code.net>
2015-04-22 12:02:26 +0200

    using the users service

--- hello.go
+++ hello.go
@@ -3,6 +3,9 @@ package hello
 import (
 	"fmt"
 	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/user"
 )
 
 func init() {
@@ -10,5 +13,20 @@ func init() {
 }
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello, World! Have you seen the hot-reload yet?")
+	context := appengine.NewContext(request)
+	currentUser := user.Current(context)
+
+	if currentUser == nil {
+		url, err := user.LoginURL(context, request.URL.String())
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		writer.Header().Set("Location", url)
+		writer.WriteHeader(http.StatusFound)
+		return
+	}
+
+	fmt.Fprintf(writer, "Hello, %v!", currentUser)
 }
